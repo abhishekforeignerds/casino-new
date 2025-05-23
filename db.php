ba8648cdc5a2529c7badbd7e115281d4e37510dd
abhishekforeignerds <abhishek.foreignerds.com>
2025-05-23 20:02:10 +0530

    adding app signinapi

--- db.php
+++ db.php
@@ -1,21 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root"; // Change to your database username
-$pass = "";     // Change to your database password
-$dbname = "casino-game";
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
-
-<?php
 // $host = "localhost";
-// $user = "u861477996_casinogame"; // Change to your database username
-// $pass = "F~m985!v0";     // Change to your database password
-// $dbname = "u861477996_casinogame";
+// $user = "root"; // Change to your database username
+// $pass = "";     // Change to your database password
+// $dbname = "casino-game";
 
 // $conn = new mysqli($host, $user, $pass, $dbname);
 
@@ -23,3 +10,17 @@ if ($conn->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
 // }
 ?>
+
+<?php
+$host = "localhost";
+$user = "u861477996_casinogame"; // Change to your database username
+$pass = "F~m985!v0";     // Change to your database password
+$dbname = "u861477996_casinogame";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//chaged
