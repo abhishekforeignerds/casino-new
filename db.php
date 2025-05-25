ac9ab9425732f6126b5b888a776a69fdf912db43
abhishekforeignerds <abhishek.foreignerds.com>
2025-05-25 10:30:56 +0530

    print tickets

--- db.php
+++ db.php
@@ -1,25 +1,24 @@
 <?php
-$host = "localhost";
-$user = "root"; // Change to your database username
-$pass = "";     // Change to your database password
-$dbname = "casino-game";
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
-
-<?php
-// $host = "localhost";
-// $user = "u861477996_casinogame"; // Change to your database username
-// $pass = "F~m985!v0";     // Change to your database password
-// $dbname = "u861477996_casinogame";
+// $// $host = "localhost";
+// $user = "root"; // Change to your database username
+// $pass = "";     // Change to your database password
+// $dbname = "casino-game";
 
 // $conn = new mysqli($host, $user, $pass, $dbname);
 
 // if ($conn->connect_error) {
 //     die("Connection failed: " . $conn->connect_error);
 // }
+?>
 
+<?php
+$host = "localhost";
+$user = "u861477996_casinogame"; // Change to your database username
+$pass = "F~m985!v0";     // Change to your database password
+$dbname = "u861477996_casinogame";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
