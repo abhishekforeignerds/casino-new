89455818724ec0bb836870761e39a3e372f1f2db
abhishekforeignerds <abhishek.foreignerds.com>
2025-05-27 00:31:43 +0530

    new changes

--- Casino-Admin/resources/js/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-console.log(`Current Vite mode: ${import.meta.env.MODE}`);
-console.log("VITE_APP_ENV:", import.meta.env.VITE_APP_ENV);
-console.log("VITE MODE:", import.meta.env.MODE);
--- Casino-Admin/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\TicketApiController;
-
-Route::get('/tickets/latest', [TicketApiController::class, 'latest']);
-Route::get('/tickets/latest-escpos', [TicketApiController::class, 'latestEscPos']);
